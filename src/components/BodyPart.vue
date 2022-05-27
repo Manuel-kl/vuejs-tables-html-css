a99bfb3de5cd9d9131747e6e11af991a6b1fdb9d
Langat Emmanuel <87006471+Manuel-kip@users.noreply.github.com>
2022-05-28 01:07:40 +0300

    tables vuejs

---
+++ src/components/BodyPart.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="body">
+    <div class="title">
+      <p>My Favorite Books</p>
+    </div>
+    <div class="table">
+      <table>
+        <tr>
+          <td>Begining App Development with VueJs</td>
+          <td>Rap Payne</td>
+          <td>Apress</td>
+          <td class="star">&star;</td>
+        </tr>
+        <tr>
+          <td>VueJs For Dummies</td>
+          <td>Barry Burd</td>
+          <td>John Wiley & Sons</td>
+          <td class="star">&star;</td>
+        </tr>
+        <tr>
+          <td>VueJs Cookbook</td>
+          <td>Simone Alessandria, Brian Kayfitz</td>
+          <td>Packt Publishing Ltd</td>
+          <td class="star">&star;</td>
+        </tr>
+        <tr>
+          <td>Weekly World News</td>
+          <td>null</td>
+          <td></td>
+          <td class="star">&star;</td>
+        </tr>
+        <tr>
+          <td>VueJs for Beginners</td>
+          <td>Thomas Bailey, Alessandro Biessek, Trevor Wills</td>
+          <td>Packt Publishing Ltd</td>
+          <td class="star">&star;</td>
+        </tr>
+        <tr>
+          <td>Practical VueJs</td>
+          <td>Frank Zammetti</td>
+          <td>Apress</td>
+          <td class="star">&star;</td>
+        </tr>
+        <tr>
+          <td>VueJs in Action</td>
+          <td>Eric Windmill</td>
+          <td>Manning Publications</td>
+          <td class="star">&star;</td>
+        </tr>
+      </table>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  watch: {},
+  name: "BodyPart",
+  components: {},
+  props: {},
+  data() {
+    return {};
+  },
+  created() {},
+  methods: {},
+  mounted() {},
+};
+</script>
+<style scoped></style>
+<style lang="scss" scoped></style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="navbar">
+    <div class="back">
+      <span>&larr;</span>
+      <p>Favorite Books</p>
+    </div>
+    <div class="nav-links">
+      <a href="#">Home</a>
+      <a href="#">Favorites</a>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "NavBar",
+  components: {},
+};
+</script>
